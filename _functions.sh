bd5420794e41764c681ce990f8fe4021a5d4c2f0
Vince <vince@nstechequities.com>
2020-01-04 13:54:15 -0600

    Added new functions.

--- _functions.sh
+++ _functions.sh
@@ -298,6 +298,70 @@ log() {
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Extraction utility.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+extract() {
+   if [ -z "$1" ]; then
+      # display usage if no parameters given
+      echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+      echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
+      return 1
+   else
+      for n in $@
+      do
+         if [ -f "$n" ] ; then
+            case "${n%,}" in
+               *.tar.bz2|*.tar.gz|*.tar.xz|*.tbz2|*.tgz|*.txz|*.tar) 
+                            tar xvf "$n"       ;;
+               *.lzma)      unlzma ./"$n"      ;;
+               *.bz2)       bunzip2 ./"$n"     ;;
+               *.rar)       unrar x -ad ./"$n" ;;
+               *.gz)        gunzip ./"$n"      ;;
+               *.zip)       unzip ./"$n"       ;;
+               *.z)         uncompress ./"$n"  ;;
+               *.7z|*.arj|*.cab|*.chm|*.deb|*.dmg|*.iso|*.lzh|*.msi|*.rpm|*.udf|*.wim|*.xar)
+                            7z x ./"$n"        ;;
+               *.xz)        unxz ./"$n"        ;;
+               *.exe)       cabextract ./"$n"  ;;
+               *)           echo "extract: '$n' - unknown archive method"
+                            return 1
+                            ;;
+             esac
+         else
+            echo "'$n' - file does not exist"
+            return 1
+         fi
+      done
+   fi
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Kill a running process.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+kp() {
+   if [[ "X${1}" == "X" ]]; then
+      _error
+      echo "" 
+      _print_cyan "Nothing to kill."
+      echo ""
+      _separator
+   else
+      kill -9 `ps aux | grep -v grep | grep ${1} | awk '{print $2}'` 2>/dev/null
+      if [ $? -gt 0 ]; then
+         _error 
+         echo "" 
+	 _print_cyan "${1} process does not exist."
+	 echo ""
+	 _separator
+      else
+         _separator
+         _print_cyan "${1} process killed."
+         _separator
+      fi
+   fi
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 
 #                    M   A   I   N
 #
